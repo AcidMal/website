d9ade70616356889d7ded786710b00312984a340
AcidMal <llandynk@gmail.com>
2024-12-26 12:18:17 +0000

    Initial

---
+++ astro.config.mjs
@@ -0,0 +1,14 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+import react from '@astrojs/react';
+
+import vercel from '@astrojs/vercel';
+
+import tailwind from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [react(), tailwind()],
+  adapter: vercel()
+});
