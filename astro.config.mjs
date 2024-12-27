57f308d879b6d4613cf9fa6cf22d0a64524bbe90
AcidMal <llandynk@gmail.com>
2024-12-27 10:30:19 +0000

    Added new Tab Bar Icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,18 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-
 import react from '@astrojs/react';
-
 import vercel from '@astrojs/vercel';
-
 import tailwind from '@astrojs/tailwind';
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  adapter: vercel()
+  adapter: vercel(),
+  output: 'static',  // Ensure static file output
+  build: {
+    assets: 'assets'  // This is the default, but let's make it explicit
+  }
 });
+
+
