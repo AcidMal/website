acb446181e09ee17c7228a60f5542e8e5b25ca5c
AcidMal <llandynk@gmail.com>
2024-12-30 01:54:07 +1000

    Added ability to view Minecraft Server Status and Player Info

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,7 +8,7 @@ import cloudflare from '@astrojs/cloudflare'
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind(), icon()],
-  output: 'hybrid',
+  output: 'status',
   adapter: cloudflare(),
   vite: {
     build: {
