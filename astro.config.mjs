7e27bcc166f9b04576fa20c60752be61fbc07e9f
AcidMal <llandynk@gmail.com>
2025-01-01 18:23:25 +1000

    Refactor: Remove unused imports and enhance hyperlink styles in blog and about pages
    - Removed unused imports for SocialButtons and BlogPost components in about.astro and blog.astro respectively.
    - Improved hyperlink styles in welcome.astro for better user experience.

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,7 +3,6 @@ import react from '@astrojs/react';
 import vercel from '@astrojs/vercel';
 import tailwind from '@astrojs/tailwind';
 import icon from 'astro-icon';
-import cloudflare from '@astrojs/cloudflare';
 
 export default defineConfig({
   integrations: [react(), tailwind(), icon()],
